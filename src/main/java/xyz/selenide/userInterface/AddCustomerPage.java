793ae22aba5fe29579f3d118577d6af3d555e9fc
Donatas Barkauskas <donatas.barkauskas@gmail.com>
2024-02-26 17:26:24 +0200

    Revert "Automated deployment: Mon Feb 26 15:14:13 UTC 2024 66cce7b9037810eebe94d66a014089e22d1227d1"
    This reverts commit 67dc66965e38bc1c5060011b7f6dd558451e8fad.

---
+++ src/main/java/xyz/selenide/userInterface/AddCustomerPage.java
@@ -0,0 +1,22 @@
+package xyz.selenide.userInterface;
+
+import com.codeborne.selenide.SelenideElement;
+import lombok.Getter;
+
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.$;
+
+@Getter
+public class AddCustomerPage {
+    private final SelenideElement inputFirstName;
+    private final SelenideElement inputLastName;
+    private final SelenideElement inputPostCode;
+    private final SelenideElement buttonAddCustomer;
+
+    public AddCustomerPage() {
+        this.inputFirstName = $(by("placeholder", "First Name"));
+        this.inputLastName = $(by("placeholder", "Last Name"));
+        this.inputPostCode = $(by("placeholder", "Post Code"));
+        this.buttonAddCustomer = $(byXpath("//button[contains(@class, 'btn-default')]"));
+    }
+}
