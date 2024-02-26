67dc66965e38bc1c5060011b7f6dd558451e8fad
donatas-b <donatas-b@users.noreply.github.com>
2024-02-26 15:14:13 +0000

    Automated deployment: Mon Feb 26 15:14:13 UTC 2024 66cce7b9037810eebe94d66a014089e22d1227d1

--- src/test/java/xyz/selenide/Tests.java
+++
@@ -1,8 +0,0 @@
-package xyz.selenide;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(tags="", features={"src/test/resources/features/manager.feature"}, glue={"xyz.selenide.stepDefinitions"}, plugin="html")
-public class Tests extends AbstractTestNGCucumberTests {
-}
